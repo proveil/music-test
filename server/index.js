37e2fc9491262ac3cbc61ff4666f23157507de04
Proveil <proveil@Nitro.localdomain>
2025-08-21 06:57:43 +0000

    deploy

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
